e0291a4a321e001f0dc7c5d6314f1b5f23dc4bd2
Lukeveille <luke@lukeleveille.com>
2019-08-25 23:40:39 -0400

    added versioning to api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   scope '/api' do
-    post '/signup' => 'users#create', :as => :user
-    post '/login' => 'authentication#authenticate'
-    put '/users' => 'users#update'
-    resources :users, except: [:index, :show, :create, :update]
-    resources :events
-    resources :attendings, only: [:index, :show, :create]
-    delete '/attendings' => 'attendings#destroy'
-    get 's3/direct_post' => 's3#direct_post'
+    scope '/v1' do
+      post '/signup' => 'users#create', :as => :user
+      post '/login' => 'authentication#authenticate'
+      put '/users' => 'users#update'
+      resources :users, except: [:index, :show, :create, :update]
+      resources :events
+      resources :attendings, only: [:index, :show, :create]
+      delete '/attendings' => 'attendings#destroy'
+      get 's3/direct_post' => 's3#direct_post'
+    end
   end
 end
